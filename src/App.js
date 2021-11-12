5cf8c0dcd58a2895b681851ca999261fbebb3791
Askar <Askarkabeer10@gmail.com>
2021-11-12 08:53:42 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+export default class App extends Component {
+  state = {
+    progress:0
+  }
+  setProgress = (progress) =>{
+    this.setState({progress:progress})
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+        <Navbar/>
+        <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+        />
+        <Switch>
+          <Route exact path="/general"><News setProgress={this.setProgress}  apiKey="c3d13209595748cebfbe4073211a0f6e"key="general" pageSize={6} country="in"category="general"/></Route>          
+          <Route exact path="/business"><News setProgress={this.setProgress}  apiKey="c3d13209595748cebfbe4073211a0f6e"key="business" pageSize={6} country="in"category="business"/></Route>
+          <Route exact path="/entertainment"><News setProgress={this.setProgress}  apiKey="c3d13209595748cebfbe4073211a0f6e"key="entertainment" pageSize={6} country="in"category="entertainment"/></Route>
+          <Route exact path="/health"><News setProgress={this.setProgress}  apiKey="c3d13209595748cebfbe4073211a0f6e"key="health" pageSize={6} country="in"category="health"/></Route>
+          <Route exact path="/science"><News setProgress={this.setProgress}  apiKey="c3d13209595748cebfbe4073211a0f6e"key="science" pageSize={6} country="in"category="science"/></Route>
+          <Route exact path="/sports"><News setProgress={this.setProgress}  apiKey="c3d13209595748cebfbe4073211a0f6e"key="sports" pageSize={6} country="in"category="sports"/></Route>
+          <Route exact path="/technology"><News setProgress={this.setProgress}  apiKey="c3d13209595748cebfbe4073211a0f6e"key="technology" pageSize={6} country="in"category="technology"/></Route>
+        </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
+
+
+
+
+
---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let {title,description,imageUrl,newsUrl,author, date} = this.props;
+        return (
+            <div className="my-3">
+                <div className="card" >
+                    <img src={imageUrl!=null?imageUrl:"https://images.hindustantimes.com/auto/img/2021/11/07/600x338/Honda_ZR-V_compact_SUV_1_1636253922969_1636253934349.jpeg"} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <div className="card-footer">
+                            <small className="text-muted">By {!author?"Unknown":author} On {new Date(date).toGMTString()}</small>
+                        </div>
+                        <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
